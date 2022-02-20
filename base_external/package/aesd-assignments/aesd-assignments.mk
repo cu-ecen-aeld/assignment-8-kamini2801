cfa3e9a1c3b1387f2828904310c342a8a63e73d2
kamini2801 <kamini.budke@colorado.edu>
2022-02-20 02:57:58 -0700

    feat: modified submodule reference

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,8 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '0b78e4d09bf1964f23eefa27324d3f5994927b76'
-AESD_ASSIGNMENTS_OVERRIDE_SRCDIR= "/home/kamini/AESD/assignments-3-and-later-kamini2801"
+AESD_ASSIGNMENTS_VERSION = '1f5651dc2278bb5fc820128f47bfc9822347630d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
