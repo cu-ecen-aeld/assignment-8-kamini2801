5d97bc678d9a3b81ed3e5cf46885d35d2855887a
kamini2801 <kamini.budke@colorado.edu>
2022-03-14 13:26:05 -0600

    feat: load ldd driver, status: cannot load modules

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+
 AESD_ASSIGNMENTS_VERSION = '1f5651dc2278bb5fc820128f47bfc9822347630d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
@@ -15,16 +15,20 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 AESD_ASSIGNMENTS_OVERRIDE_SRCDIR = /home/kamini/AESD/assignments-3-and-later-kamini2801
 
+AESD_ASSIGNMENTS_MODULE_SUBDIRS += aesd-char-driver/
+
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment5/* $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/server/Start_script.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/server/my_test $(TARGET_DIR)/usr/bin
+
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment8-buildroot/* $(TARGET_DIR)/usr/bin
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -3,17 +3,17 @@
 case "$1" in 
 	start) 
 		echo "Loading custom module"
-        #/usr/bin/module_load faulty
-		#/usr/bin/scull_load
+        /usr/bin/module_load faulty
+		/usr/bin/scull_load
 		/usr/bin/aesdchar_load
-        #modprobe hello
+        modprobe hello
 		;; 
 	stop) 
 		echo "Unloading custom module" 
-        #/usr/bin/scull_unload
-        #/usr/bin/module_unload faulty
+        /usr/bin/scull_unload
+        /usr/bin/module_unload faulty
 		/usr/bin/aesdchar_unload
-		#rmmod hello
+		rmmod hello
 		;; 
 	*) 
 		echo "Usage: $0 {start|stop}" 
